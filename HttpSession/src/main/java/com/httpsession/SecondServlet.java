d433ec01372f50748df3c6937703c5d715ef8c8b
Praveen <praween.golla@gmail.com>
2023-01-12 08:17:13 +0530

    First HttpSession Commit

---
+++ HttpSession/src/main/java/com/httpsession/SecondServlet.java
@@ -0,0 +1,42 @@
+package com.httpsession;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session = request.getSession(false);
+		String name = (String) session.getAttribute("name");
+		out.println("Hello "+name);
+		
+		out.println();
+	}
+
+}
